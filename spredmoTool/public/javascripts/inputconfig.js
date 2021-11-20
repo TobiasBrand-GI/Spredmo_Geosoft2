867dac1de54520b52486205f5299cbe2498beb9a
TobiasBrand-GI <tobias.brand1@web.de>
2021-11-20 13:05:13 +0100

    different input options

--- spredmoTool/public/javascripts/inputconfig.js
+++ spredmoTool/public/javascripts/inputconfig.js
@@ -1,14 +1,37 @@
+
 changeInputHTML("model");
 
 function changeInputHTML(choice){
     let div=document.getElementById("modelInDiv");
     div.innerHTML="";
     if(choice=="model"){
-        let t= document.createElement("input");
-        div.appendChild(t);
+        createFileUpload("Wählen Sie ihr Modell aus", div);
     }
     else{
-        let t= document.createElement("button");
-        div.appendChild(t);
+        createFileUpload("Wählen Sie ihre Trainingsdaten aus", div);
+        let p = document.createElement("INPUT");
+        p.setAttribute("type","number");
+        let br= document.createElement("br");
+        div.appendChild(br);
+        div.appendChild(p);
     }
+}
+function createFileUpload(placeholder, div){
+    let d= document.createElement("DIV");
+    d.setAttribute("class", "custom-file");
+
+    let f= document.createElement("INPUT");
+    f.setAttribute("type", "file");
+    f.setAttribute("name","modelFile")
+    f.setAttribute("class", "custom-file-input");
+
+    var l = document.createElement("LABEL");
+    l.htmlFor="modelFile";
+    l.innerHTML=placeholder;
+    l.setAttribute("class", "custom-file-label")
+
+    d.appendChild(f);
+    d.appendChild(l);
+    div.appendChild(d);
+
 }
