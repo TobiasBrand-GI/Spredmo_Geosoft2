3d6e5a617cc4e00727fc33b1b1691878cbf0930a
TobiasBrand-GI <tobias.brand1@web.de>
2021-12-14 11:05:11 +0100

    added download and impressum pages

--- spredmoTool/public/javascripts/inputconfig.js
+++ spredmoTool/public/javascripts/inputconfig.js
@@ -12,10 +12,10 @@ function changeInputHTML(choice, status){
         div.innerHTML="";
     }
     if(choice=="model"){
-        createFileUpload("Wählen Sie ihr Modell aus", div);
+        createFileUpload("Dieses Modell", div);
     }
-    else{
-        createFileUpload("Wählen Sie ihre Trainingsdaten aus", div);
+    else if(choice=="data"){
+        createFileUpload("Diese Trainingsdaten", div);
         let p = document.createElement("INPUT");
         p.setAttribute("type","number");
         let br= document.createElement("br");
@@ -31,19 +31,69 @@ function changeInputHTML(choice, status){
  */
 function createFileUpload(placeholder, maindiv){
     let d= document.createElement("DIV");
-    d.setAttribute("class", "custom-file");
+    d.setAttribute("class", "input-group");
 
     let f= document.createElement("INPUT");
     f.setAttribute("type", "file");
-    f.setAttribute("name","modelFile")
-    f.setAttribute("class", "custom-file-input");
+    f.setAttribute("name","modelFile");
+    f.setAttribute("class", "form-control form-control-lg");
 
-    var l = document.createElement("LABEL");
+    /*let l = document.createElement("LABEL");
     l.htmlFor="modelFile";
     l.innerHTML=placeholder;
-    l.setAttribute("class", "custom-file-label")
+    l.setAttribute("class", "custom-file-label")*/
+
+    let bd=document.createElement("DIV");
+    bd.setAttribute("class", "input-group-append");
+
+    let b=document.createElement("BUTTON");
+    b.innerHTML=placeholder+" verwenden";
+    b.id="readButton";
+    b.setAttribute("class", "btn btn-green");
+
+    bd.appendChild(b);
 
     d.appendChild(f);
-    d.appendChild(l);
+    //d.appendChild(l);
+    d.appendChild(bd);
     maindiv.appendChild(d);
 }
+/**
+ * Functionality to add Pois per JSON-File
+ */
+ document.querySelector("#readButton").addEventListener('click', function() {
+    //catching empty files
+    if(document.querySelector("#modelfile").files.length == 0) {
+      alert('Error : No file selected');
+      return;
+    }
+    // file selected by user
+    let file = document.querySelector("#modelfile").files[0];
+  
+    // file MIME type
+    let filename = (file.name).split(".");
+    let file_type = filename[1];
+    console.log(file_type)
+    if(file_type=="json" || file_type=="geojson"){
+      // new FileReader object
+      let reader = new FileReader();
+  
+      // event fired when file reading finished
+      reader.addEventListener('load', function(e) {
+        // contents of the file
+          let text = e.target.result;
+          document.querySelector("#jsonText").textContent = text;
+          document.getElementById("jsonText").value=text;
+      });
+  
+      // event fired when file reading failed
+      reader.addEventListener('error', function() {
+          alert('Error : Failed to read file');
+      });
+  
+      // read file as text file
+      reader.readAsText(file);
+    }else{
+      alert('Error : Wrong File format selected');
+    }
+  })
