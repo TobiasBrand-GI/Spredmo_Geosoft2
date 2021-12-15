c2abef4c5f0997c8a13a04bf8fad7d2d08308aaa
TobiasBrand-GI <tobias.brand1@web.de>
2021-12-15 15:31:45 +0100

    added images

--- spredmoTool/public/javascripts/geoTiff.js
+++ spredmoTool/public/javascripts/geoTiff.js
@@ -1,16 +1,19 @@
 
 const plottyRenderer = new L.LeafletGeotiff.plotty({
-    displayMin: 0,
-    displayMax: 10,
+    displayMin: -100,
+    displayMax: 100,
     clampLow: false,
     clampHigh: false,
   });
 
 function loadTIFF(){
-    const url ="../images/wind_speed.tif";
+    const windurl ="../images/wind_direction.tif";
+    //const ownurl ="../images/differenz_waldflaeche.tif"
     const options={renderer:plottyRenderer,};
-    var tifflayer = new L.LeafletGeotiff(url, options).addTo(mymap);
+    var tifflayer = new L.LeafletGeotiff(windurl, options).addTo(mymap);
+    //var owntifflayer = new L.LeafletGeotiff(ownurl, options).addTo(mymap);
     console.log(tifflayer);
+    //console.log(owntifflayer);
 }
 
 loadTIFF();
