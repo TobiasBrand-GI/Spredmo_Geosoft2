acb70dcf5cebcdc95d297353daa61f0f790c25b0
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-12-21 19:20:55 +0100

    installtion of packages and load packages

---
+++ R-folder/installs.r
@@ -0,0 +1,38 @@
+## Installations
+## Tutorials:
+## https://r-spatial.org/r/2021/04/23/cloud-based-cubes.html
+## https://appelmar.github.io/ogh2021/tutorial.html
+
+#installtion of packages
+
+install.packages("sf")
+install.packages("sp")
+install.packages("raster")
+install.packages("caret")
+install.packages("viridis")
+install.packages("latticeExtra")
+install.packages("randomForest")
+install.packages("CAST")
+install.packages("doParallel")
+# install.packages("parallel")
+# install.packages("iterators")
+
+
+#load package
+
+library(sf)
+library(sp)
+library(raster)
+library(ggplot2)
+library(lattice)
+library(caret)
+library(viridisLite)
+library(viridis)
+library(latticeExtra)
+library(randomForest)
+library(CAST)
+library(foreach)
+library(iterators)
+library(parallel)
+library(doParallel)
+
