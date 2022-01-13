2fc1a6ea5d84fbbb43b8d7d1541f31487a7f7ea5
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2022-01-13 18:05:27 +0100

    fix problems with `raster_cube()`

--- R-folder/stac_script.r
+++ R-folder/stac_script.r
@@ -50,23 +50,23 @@ message("DONE: stac_search()")
 ### Creating an image collection with a filter
 library(gdalcubes)
 # reference for meaning of bands: https://gdal.org/drivers/raster/sentinel2.html
-assets = c("B01","B02","B03","B04","B05","B06", "B07","B08","B8A","B09","B11","B12","SCL")
+assets <- c("B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11","B12","SCL")
 s2_collection = stac_image_collection(items$features, 
                                       asset_names = assets, 
                                       property_filter = function(x) {
                                         x[["eo:cloud_cover"]] < 20})
-# s2_collection
+s2_collection
 message("DONE: stac_image_collection()")
 
 #####
 ### generate data cube
-cube_view_input_shape = cube_view(srs="EPSG:32632",  
-                                   dx = 20, 
-                                   dy = 20, 
-                                   dt = "P30D", 
-                                   aggregation = "median", 
+cube_view_input_shape <- cube_view(srs = "EPSG:32632",
+                                   dx = 20,
+                                   dy = 20,
+                                   dt = "P30D",
+                                   aggregation = "median",
                                    resampling = "average",
-                                   extent = list(t0 = "2018-06-01", 
+                                   extent = list(t0 = "2018-06-01",
                                                  t1 = "2018-06-30",
                                                  left = bbox["xmin"]-1000, 
                                                  right = bbox["xmax"]+1000,
@@ -77,12 +77,12 @@ message("DONE: cube_view()")
 
 #####
 ### mask for clouds and their shadows
-s2_mask = image_mask("SCL", values = c(3,8,9))
+s2_mask <- image_mask("SCL", values = c(3,8,9))
 message("DONE: image_mask()")
 
 #####
 ### set threads (logische Prozessoren) 
-#library(magrittr)
+library(magrittr)
 gdalcubes_options(threads = 6)
 message("DONE: set threads")
 
@@ -90,9 +90,6 @@ message("DONE: set threads")
 #####
 ### make raster cube
 library(dplyr) # needed for '%>%'
-# TODO: try catch wegen geom/geometry
-# im fs package ist evtl eine Methode, um herauszufinden,
-# ob das `geom` oder `geometry` heisst
 message("DO NOT WORRY :)")
 message("this raster cube function takes some time:")
 if (!is.null(input_shape_32632$geometry)){
@@ -102,21 +99,31 @@ if (!is.null(input_shape_32632$geometry)){
 }
 warning("!!! check path !!!")
 satelite_cube <- raster_cube(s2_collection, cube_view_input_shape, s2_mask) %>%
-  #select_bands(c("B02", "B03", "B04")) %>%
-  select_bands(c("B02","B03","B04","B08","B11")) %>% #B, G, R, NIR, SWIR
-  apply_pixel("(B08-B04)/(B08+B04)", "NDVI", keep_bands = TRUE) %>% #NDVI - Normalized Difference Vegetation Index
-  apply_pixel("(B11+B04)-(B08+B02)/(B11+B04)+(B08+B02)", "BSI", keep_bands = TRUE) %>% #Bare Soil Index
-  apply_pixel("(B04 + 0.3)/(B03+B11)", "BAEI", keep_bands = TRUE) %>% #Built-up Area Extraction Index
-  reduce_time(c("median(B02)", 
+  # select bands B, G, R, NIR, SWIR
+  select_bands(c("B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11","B12","SCL")) %>% 
+  # NDVI - Normalized Difference Vegetation Index
+  apply_pixel("(B08-B04)/(B08+B04)", "NDVI", keep_bands = TRUE) %>% 
+  # Bare Soil Index
+  apply_pixel("(B11+B04)-(B08+B02)/(B11+B04)+(B08+B02)", "BSI", keep_bands = TRUE) %>% 
+  # Built-up Area Extraction Index
+  apply_pixel("(B04 + 0.3)/(B03+B11)", "BAEI", keep_bands = TRUE) %>% 
+  reduce_time(c("median(B04)",
+                "median(B02)",
                 "median(B03)", 
-                "median(B04)", 
+                "median(B01)",
+                "median(B05)",
+                "median(B06)", 
+                "median(B07)", 
                 "median(B08)",
-                "median(B11)",
+                "median(B8A)", 
+                "median(B09)",
+                "median(B11)", 
+                "median(B12)", 
                 "median(NDVI)", 
                 "median(BSI)", 
                 "median(BAEI)")) %>%
   filter_geom(temp) %>%
-  # plot(rgb = 3:1, zlim=c(0,1500)) #%>% # write_tif() does not work when using plot() here 
+  # plot(rgb = 3:1, zlim=c(0,1500)) %>%        # write_tif() does not work when using plot() here 
   write_tif(dir = "C:/Users/.../GitHub/Spredmo_Geosoft2/R-folder",
             prefix = "output_",
             overviews = FALSE,
@@ -132,6 +139,6 @@ message("DONE: save as geoTiff")
 ### printing processing time
 end_time <- Sys.time()
 time_differnece <- paste("total processing time: ", 
-                         round(100000*(end_time - start_time))/100000, 
+                         round(100000 * (end_time - start_time)) / 100000, 
                          " Minutes")
 print(time_differnece)
