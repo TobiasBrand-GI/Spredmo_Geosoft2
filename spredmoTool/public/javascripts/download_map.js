ba0b00986909af097c0a282ec62a19e027bb25b8
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2022-01-19 16:11:43 +0100

    cleaned up

--- spredmoTool/public/javascripts/download_map.js
+++ spredmoTool/public/javascripts/download_map.js
@@ -1,27 +1,28 @@
-'use strict'
 /**
  * Basemap for Leaflet Application
  * @type Leaflet Map
  */
- //let dmap = L.map('mapid').setView([51.965, 7.63],13);
- L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
- }).addTo(dmap);
+var dmap = L.map('mapid').setView([0, 0], 5);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(dmap);
 
-var url_to_geotiff_file = "wind_direction.tif";
+function loadTIFF(url){
+  var url_to_geotiff_file =url;
 
-fetch(url_to_geotiff_file)
-  .then(response => response.arrayBuffer())
-  .then(arrayBuffer => {
-    parse_georaster(arrayBuffer).then(georaster => {
-      console.log("georaster:", georaster);
-      var layer = new GeoRasterLayer({
-          georaster: georaster,
-          opacity: 0.7,
-          pixelValuesToColorFn: values => values[0] > 100 ? '#ff0000' : '#0000ff',
+  fetch(url_to_geotiff_file)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => {
+      parseGeoraster(arrayBuffer).then(georaster => {
+        console.log("georaster:", georaster);
+        var layer = new GeoRasterLayer({
+            georaster: georaster,
+            opacity: 0.7,
+            pixelValuesToColorFn: values => values[0] > 100 ? '#ff0000' : '#0000ff',
           resolution: 64 // optional parameter for adjusting display resolution
-      });
-      layer.addTo(map);
-      map.fitBounds(layer.getBounds());
+        });
+        layer.addTo(dmap);
+        dmap.fitBounds(layer.getBounds());
+    });
   });
-});
+}
