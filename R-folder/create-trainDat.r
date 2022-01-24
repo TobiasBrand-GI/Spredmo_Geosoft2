095676f82fc876e12a7eee1ae468ffd38f64d754
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2022-01-24 23:19:55 +0100

    check and correct Iiput parameters

--- R-folder/create-trainDat.r
+++ R-folder/create-trainDat.r
@@ -13,14 +13,14 @@ library(gdalcubes)
 #####
 ### get right crs
 find_right_crs <- function(input_area) {
-  longitude <- st_bbox(input_area)[1]
-  utm_zone <- (floor((longitude + 180)/6) %% 60) + 1
-  targetString <- paste('EPSG:326',utm_zone, sep = "")
+  #longitude <- st_bbox(input_area)[1]
+  #utm_zone <- (floor((longitude + 180)/6) %% 60) + 1
+  #targetString <- paste('EPSG:326',utm_zone, sep = "")
+  # it only works with following EPSG-code 
+  targetString <- paste('EPSG:4326')
   message("DONE: get right crs")
   return(targetString)
 }
-
-
 
 
 #####
@@ -37,13 +37,13 @@ calculate_bbox <- function(input_sites, in_epsg) {
 
 #####
 ### stac-request
-get_sentinelDat_form_stac <- function(input_bbox) {
+get_sentinelDat_form_stac <- function(input_bbox, in_t0, in_t1) {
     s = stac("https://earth-search.aws.element84.com/v0")
     items = s %>%
         stac_search(collections = "sentinel-s2-l2a-cogs",
                     bbox = c(input_bbox["xmin"],input_bbox["ymin"],
                             input_bbox["xmax"],input_bbox["ymax"]),
-                    datetime = "2018-06-01/2018-06-30",
+                    datetime = paste(in_t0, "/", in_t1, sep = ""),
                     limit = 500) %>%
         post_request()
     message("DONE: stac_search()")
@@ -54,13 +54,13 @@ get_sentinelDat_form_stac <- function(input_bbox) {
 #####
 ### Creating an image collection with a filter
 # reference for meaning of bands: https://gdal.org/drivers/raster/sentinel2.html
-create_filtered_image_collection <- function(input_items) {
+create_filtered_image_collection <- function(input_items, intpu_cloud_coverage) {
     assets <- c("B02","B03","B04","B08","B06","B07","B8A","B11","B12","SCL")
-    #c("B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11","B12","SCL")
+    # assets <- c("B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11","B12","SCL")
     s2_collection <- stac_image_collection(input_items$features,
                                     asset_names = assets, 
                                     property_filter = function(x) {
-                                        x[["eo:cloud_cover"]] < 20})
+                                        x[["eo:cloud_cover"]] < intpu_cloud_coverage})
     message("DONE: create_image_collection()")
     return(s2_collection)
 }
@@ -70,13 +70,13 @@ create_filtered_image_collection <- function(input_items) {
 ### generate data cube
 generate_cube_view <- function(input_epsg_string, in_dx, in_dy, in_t0, in_t1, in_bbox) {
     cube_view_for_data_cube <- cube_view(srs = input_epsg_string,
-                                        dx = in_dx, # 200, #20,
-                                        dy = in_dy, # 200, #20,
+                                        #dx = in_dx, # 200, #20,
+                                        #dy = in_dy, # 200, #20,
                                         dt = "P30D",
                                         aggregation = "median",
                                         resampling = "average",
-                                        extent = list(t0 = in_t0, # "2018-06-01",
-                                                    t1 = in_t1, # "2018-06-30",
+                                        extent = list(t0 = in_t0,
+                                                    t1 = in_t1,
                                                     left = in_bbox[1]-1000, # xmin
                                                     right = in_bbox[2]+1000, # ymin
                                                     top = in_bbox[3] + 1000, # xmax
@@ -112,16 +112,13 @@ generate_raster_cube <- function(input_area, input_collection, input_cube_view, 
   #print(input_cube_view$space$srs)
   #print(input_image_mask)
   #print(input_epsg)
-  
-  
     library(dplyr) # needed for '%>%'
-    message("DO NOT WORRY :)")
     if (!is.null(input_area$geometry)){
         temp <- input_area$geometry
     } else  temp <- input_area$geom
-    satelite_cube <- raster_cube(input_collection, input_cube_view, input_image_mask)
-    print(satelite_cube)
-    filter_geom(satelite_cube, temp) #, srs = input_epsg)
+    satelite_cube <- raster_cube(input_collection, input_cube_view, input_image_mask) 
+    # print(satelite_cube)
+    # filter_geom(temp, srs = input_epsg)
     message("DONE: raster_cube()")
     return(satelite_cube)
 }
@@ -131,7 +128,11 @@ generate_raster_cube <- function(input_area, input_collection, input_cube_view, 
 ### save data from data cube as GeoTiff
 save_data_as_geoTiff <- function(input_cube, input_storage_path, input_prefix) {
     warning("!!! check path !!!")
+    message("DO NOT WORRY :)")
     message("this saving function takes some time:")
+#    write.asciigrid(cube_for_trainingSites, "C:/Users/49157/Documents/GitHub/Spredmo_Geosoft2/R-folder/test.grd")
+    
+  
     write_tif(input_cube,
             dir = input_storage_path,
             prefix = input_prefix,
@@ -145,29 +146,44 @@ save_data_as_geoTiff <- function(input_cube, input_storage_path, input_prefix) {
 }
 
 
-
-
-
+#####
+### Raster data (predictor variables)
+load_predictors_and_rename_bands <- function() {
+    warning("!!! check path !!!")
+    # sen_ms <- stack(paste(input_storage_path, input_prefix))
+    sentinel <- stack("C:/Users/49157/Documents/GitHub/Spredmo_Geosoft2/R-folder/satelite_for_trainingSites__2018-06-01.tif")
+    # rename bands
+    names(sentinel) <- c("B02","B03","B04","B08","B06","B07","B8A","B11","B12","SCL")
+    # plot(sen_ms)
+    # plotRGB(sentinel,stretch="lin",r=3,g=2,b=1)
+    message("DONE: load as RasterStack")
+    return(sentinel)
+}
 
 
 #####
 ### Daten kombinieren
-extr <- extract(sentinel,trainingSites,df=TRUE)
-head(extr)
-trainingSites$PolyID <- 1:nrow(trainingSites) 
-extr <- merge(extr,trainingSites,by.x="ID",by.y="PolyID")
-head(extr)
-saveRDS(extr,file="trainingsites/trainData.RDS")
+combine_sentinel_with_trainingSites <- function(input_trainingSites, predictors_stack) {
+    extr <- extract(predictors_stack, input_trainingSites, df=TRUE)
+    head(extr)
+    input_trainingSites$ClassID <- 1:nrow(input_trainingSites) 
+    extr <- merge(extr,input_trainingSites,by.x="ID",by.y="ClassID")
+    head(extr)
+    saveRDS(extr,file="trainingsites/trainData.RDS")
+}
 
 
 #####
 ### parameters from plumber APIs:
-trainingSites <- read_sf('C:/Users/49157/Documents/FS_5_WiSe_21-22/M_Geosoft_2/geodata_tests/input_test_mit_thomas_1.gpkg')
+# trainingSites <- read_sf('C:/Users/49157/Documents/FS_5_WiSe_21-22/M_Geosoft_2/geodata_tests/aoi_jena.gpkg') ##input_test_mit_thomas_1.gpkg')
+trainingSites <- read_sf("C:/Users/49157/Documents/GitHub/Spredmo_Geosoft2/R-folder/tests/testdata.geojson")
 resolution_x <- 200
 resolution_y <- 200
-start_day <- "2018-06-01"
-end_day <- "2018-06-30"
+start_day <- "2021-04-01"
+end_day <- "2021-04-30"
+cloud_coverage <- 80
 path_for_satelite_for_trainingSites = "C:/Users/49157/Documents/GitHub/Spredmo_Geosoft2/R-folder"
+
 
 
 #####
@@ -177,14 +193,16 @@ set_threads()
 
 
 #####
-### function calls in case of untrained model
+### function calls in case of no model
 fitting_epsg_as_string <- find_right_crs(trainingSites)
 bbox_wgs84 <- calculate_bbox(trainingSites, fitting_epsg_as_string)
-sentinelDat <- get_sentinelDat_form_stac(bbox_wgs84)
+sentinelDat <- get_sentinelDat_form_stac(bbox_wgs84, start_day, end_day)
 trainingSites <- st_transform(trainingSites, crs=fitting_epsg_as_string)
-image_collection_for_trainingSites <- create_filtered_image_collection(sentinelDat)
-cube_view_for_trainingSites <- generate_cube_view(fitting_epsg_as_string, resolution_x, resolution_x, start_day, end_day, bbox_wgs84)
+image_collection_for_trainingSites <- create_filtered_image_collection(sentinelDat, cloud_coverage)
+cube_view_for_trainingSites <- generate_cube_view(fitting_epsg_as_string, resolution_x, resolution_y, start_day, end_day, bbox_wgs84)
 cube_for_trainingSites <- generate_raster_cube(trainingSites, image_collection_for_trainingSites, cube_view_for_trainingSites, image_mask_for_data_cube, fitting_epsg_as_string)
 prefix_for_geoTiff_for_trainingSites = "satelite_for_trainingSites__"
 save_data_as_geoTiff(cube_for_trainingSites, path_for_satelite_for_trainingSites, prefix_for_geoTiff_for_trainingSites)
 
+predictors_stack <- load_predictors_and_rename_bands()
+trainingDat <- combine_sentinel_with_trainingSites(trainingSites, predictors_stack) # can't test this
