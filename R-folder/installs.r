552c047be985267f094dde26443672d2a592686e
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2022-01-31 22:33:11 +0100

    update installation file for r-packages :)

--- R-folder/installs.r
+++ R-folder/installs.r
@@ -5,35 +5,78 @@
 
 #installtion of packages
 
+# install.packages("sf")
+# install.packages("sp")
+# install.packages("raster")
+# install.packages("caret")
+# install.packages("viridis")
+# install.packages("latticeExtra")
+# install.packages("randomForest")
+# install.packages("CAST")
+# install.packages("doParallel")
+# # install.packages("parallel")
+# # install.packages("iterators")
+
+
+# #load package
+
+# library(sf)
+# library(sp)
+# library(raster)
+# library(ggplot2)
+# library(lattice)
+# library(caret)
+# library(viridisLite)
+# library(viridis)
+# library(latticeExtra)
+# library(randomForest)
+# library(CAST)
+# library(foreach)
+# library(iterators)
+# library(parallel)
+# library(doParallel)
+# library(gdalcubes)
+
+
+
+#####
+### status quo:
 install.packages("sf")
 install.packages("sp")
 install.packages("raster")
 install.packages("caret")
-install.packages("viridis")
-install.packages("latticeExtra")
-install.packages("randomForest")
+install.packages("terra")
+install.packages("gdalcubes")
+install.packages("rstac")
 install.packages("CAST")
+install.packages("geosphere")
+install.packages("jsonlite")
+install.packages("rgeos")
+install.packages("rgdal")
+install.packages("latticeExtra")
 install.packages("doParallel")
-# install.packages("parallel")
-# install.packages("iterators")
+install.packages("parallel")
+install.packages("iterators")
+install.packages("foreach")
+install.packages("magrittr")
 
 
-#load package
-
-library(sf)
-library(sp)
-library(raster)
-library(ggplot2)
-library(lattice)
-library(caret)
-library(viridisLite)
-library(viridis)
-library(latticeExtra)
-library(randomForest)
-library(CAST)
+library(raster)#
+library(sf)#
+library(rstac)#
+library(gdalcubes)#
+library(sp)#
+library(terra)#
+library(caret)#
+library(CAST)#
+library(geosphere)
+library(jsonlite)
+library(rgeos)
+library(rgdal)
+### additional required packages:
+library(latticeExtra)#
 library(foreach)
-library(iterators)
-library(parallel)
-library(doParallel)
-library(gdalcubes)
-
+library(iterators)#
+library(doParallel)#
+library(parallel)#
+library(magrittr)
