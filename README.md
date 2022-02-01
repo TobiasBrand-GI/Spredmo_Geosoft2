faf705cbf6419a7317d9dda36fea64939c01401c
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2022-02-01 10:27:47 +0100

    Readme stuff

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Our software supports multiple devices and operating systems due to the usage of
 To install the "Spredmo-Tool" on your AWS EC2 instance, you need to:
 + Install Docker for servers
 + Install an R work environment on your instance.
-+ Install the R packages
 + Start up the Docker Container
+
+For the Frontend part of the app, you need to install Docker on your local machine or web-hosting server and simply run the Docker Container.
+Due to security reasons, we cannot include a working identification key for connecting with the AWS. So to be able to connect to your instance,
+start your Docker container and run ```docker exec -it <container name> /bin/bash``` and copy a valid == key.pem == into the ```SPREDMO_GEOSOFT2/spredmoTool/keys ``` folder.
--- spredmoTool/routes/plumber.js
+++ spredmoTool/routes/plumber.js
@@ -1,18 +1,19 @@
-var express = require('express');
+var express = require('express'); // Express routing middleware
 var router = express.Router();
-const axios = require('axios');
-const multer = require('multer');
-const scp = require('scp')
-const fs = require('fs');
+const axios = require('axios');   // Library for http requests
+const multer = require('multer'); // Library for local file management
+const scp = require('scp')        // Library for connecting and exchanging files with an AWS instance via scp
 const { default: Client } = require('node-scp');
-const { send } = require('process');
-const gjv = require("geojson-validation");
+const fs = require('fs');         // Library for file system functionality
+const gjv = require("geojson-validation"); // GeoJSON Validator
 
+// Variables for saving input data between API calls
 let fileNames = new Array();
 let serverFileNames = new Array();
 let ui_Body;
 let fileName;
 
+// Option stack for multer library
 var storage = multer.diskStorage({
   destination: function (request, file, callback) {
       callback(null, './tmp');
@@ -24,56 +25,82 @@ var storage = multer.diskStorage({
 });
 
 const uploadDest = multer({storage:storage})
+
+/**
+ * API route to get the file selected by the user and load it into the Node.js environment for further use.
+ * After loading is finished, it redirects to the download/ results page.
+ */
 router.post('/upload', uploadDest.single('modelFile'), function(req, res) {
   ui_Body=req.body; 
-  fileNames.push(fileName);
+  fileNames.push(fileName); // Saving file name for later use
   res.redirect("/download.html")
 })
 
+/**
+ * API call to send all necessary parameters and files via scp and axios to the Plumber API and AWS instance.
+ * Validates user input. If validation fail, redirects to the index page.
+ * Fetch the result URLs and download the files.
+ */
 router.get('/results',function(req, res) {
   let radioButton = ui_Body.modelInput;
   let mime = fileName.split(".")[1];
   try{
-    if(gjv.valid(JSON.parse(ui_Body.geoJSONInput))===false){
+    // Validate GeoJSON of area of interest
+    if(gjv.valid(JSON.parse(ui_Body.geoJSONInput))===false){  
       res.json({success:false, message:"Your area of interest GeoJSON Code was invalid!"})
-    }else if(ui_Body.startDay>ui_Body.endDay){
+    }
+    // Check if end day was not before start day
+    else if(ui_Body.startDay>ui_Body.endDay){     
       res.json({success:false, message:"The end day must be more recent then the start day!"})
-    }else if(ui_Body.startDay<Date.now() || ui_Body.endDay<Date.now()){
+    }
+    // Check if no date is in the future
+    else if(ui_Body.startDay<Date.now() || ui_Body.endDay<Date.now()){
       res.json({success:false, message:"Chosen dates cannot be in the future!"})
-    }else{
+    }
+    // Else case if validation was successfull
+    else{
+      // Check if mode for a trained model was selected
       if(radioButton==="model"){
+        // Check for correct MIME types
         if(mime==="rds" || mime==="RDS" || mime==="rdata" || mime==="RDATA"){
+          // upload moddel file to AWS
           upload("./tmp/"+fileName, "model", mime);
           
         }else{
+          // if not correct, send json with error message
           res.json({success:false, message:"File is not an R model file in .rds or .rdata format!"})
         }
+      // Check if mode for train data was selected
       }else if(radioButton==="train"){
+        // Check for correct MIME types
         if(mime==="geojson" || mime==="GEOJSON" || mime==="gpkg" || mime==="GPKG"){
+          // Upload train data to AWS
           upload("./tmp/"+fileName, "train", mime);
         }else{
+          // if not correct, send json with error message
           res.json({success:false, message:"File is not an spatial data file in .geojson or .gpkg format!"})
         }
+      // Catch errors where no mode is selected
       }else{
         res.json({success:false, message:"No input mode was selected!"})
       }
     }
   }catch(e){
+    // Catch any error with requests, API calls and file interactions
     res.json({success:false, message:"Error: "+e})
   }
 })
-  
-router.get('/wrongInput',function(req, res) {
-  res.redirect("/index.html")
-})
 
+/**
+ * 
+ */
 async function download(){
   try{
     const client =  await Client({
     host: 'ec2-35-86-197-46.us-west-2.compute.amazonaws.com', //remote host ip 
     port: 22, //port used for scp 
     username: 'ubuntu', //username to authenticate
-    privateKey: fs.readFileSync('G:/GitHubRepositories/Spredmo_Geosoft2/spredmoTool/public/images/geosoft22021.pem'),
+    privateKey: fs.readFileSync('../keys/key.pem'), // local relative path to your pem file
   }).then(client => {
     client.downloadFile('/tmpextern/aoa.tif', 'public/images/test.tif')
       .then(response => {
@@ -86,21 +113,30 @@ async function download(){
   }
 }
 
+/**
+ * Function to upload a given local file to an AWS instance via scp. Autogenerate unique file names with a set prefix and a suffix acoording to the date.
+ * @param {String} localPath Path to the local file to be uploaded to the AWS
+ * @param {String} file Short prefix (model, train) for later name creation
+ * @param {String} type MIME type of the file which should be uploaded
+ */
 async function upload(localPath, file, type){
   try {
+    // Connection to AWS
     const client = await Client({
       host: 'ec2-35-86-197-46.us-west-2.compute.amazonaws.com', //remote host ip 
       port: 22, //port used for scp 
       username: 'ubuntu', //username to authenticate
-      privateKey: fs.readFileSync('G:/GitHubRepositories/Spredmo_Geosoft2/spredmoTool/public/images/geosoft22021.pem'),
+      privateKey: fs.readFileSync('../keys/key.pem'), // local relative path to your pem file
     })
+    // Create uniquie file name
     let uniqueStamp = createFileNames(Date.now());
     let newFileName = file+"_"+uniqueStamp+"."+type;
+    // Save file name for the Plumber code
     serverFileNames.push(newFileName);
+    // upload file
     await client.uploadFile(localPath, '/tmpextern/'+newFileName);
-    // you can perform upload multiple times
-    client.close() // remember to close connection after you finish
-
+    client.close()
+    // Delete copied file locally to free memory
     fs.unlink(localPath,(err)=>{
       console.log(err)
     })
